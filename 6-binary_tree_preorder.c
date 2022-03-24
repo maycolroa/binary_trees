ecf945ae2f10340dff85ba47dbf741bbf3f6bb2e
maycolroa <lehallo@hotmail.com>
2022-03-24 00:47:26 -0500

    0x1D. C - Binary trees  23/03/22 0.06

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_preorder - goes through a binarytree using pre-order
+ *@tree: pointer to the root.
+ *@func: pointer to a function.
+ *Return: nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+	else
+	{
+		return;
+	}
+}
